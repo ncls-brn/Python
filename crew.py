3dd7c59606dfba8aa8f8ca2942870732a35f5e6b
ncls-brn <nicolas.j.baran@gmail.com>
2025-01-08 11:13:10 +0100

    " add crew list "

--- crew.py
+++ crew.py
@@ -1,3 +1,19 @@
 print("hello world again")
 
 
+crew = [
+  {
+    "first_name": "John",
+    "last_name": "Doe",
+    "gender":"M",
+    "age": 25,
+    "role" : "Commandant"
+  },
+  {
+    "first_name": "Jane",
+    "last_name": "Doe",
+    "gender":"F",
+    "age": 24,
+    "role" : "Second commandant"
+  },
+]
